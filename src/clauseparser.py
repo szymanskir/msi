9e2a101bfa11ae0bfb240f870a826853a7f941cb
proszewskip <proszewskip@student.mini.pw.edu.pl>
2018-11-14 17:15:03 +0100

    update requirements

--- src/clauseparser.py
+++ src/clauseparser.py
@@ -1,12 +1,14 @@
 from functools import reduce
-from operator import add
+from operator import add, or_
 from typing import NamedTuple
 
-from .clause import Clause
-from .literal import Literal
-from .argument import Argument
+from clause import Clause
+from literal import Literal
+from argument import Argument
 
 class ClauseParser(NamedTuple):
+    """Class responsible for parsing input data.
+    """
     negate_marker: str = "/"
     or_splitter: str = " OR "
     and_splitter: str = " AND "
@@ -15,20 +17,28 @@ class ClauseParser(NamedTuple):
     clause_open_bracket = "["
     clause_close_bracket = "]"
 
-    def parse_cnf_list(self, clauses_list):
-        return reduce(add, set(map(self.parse_cnf, clauses_list)))
+    def parse_cnf_list(self, clauses_list: list):
+        """Parses list of sentences in CNF form.
+        """
+        return reduce(or_,(map(self.parse_cnf, clauses_list)))
 
-    def parse_cnf(self, input_string):
+    def parse_cnf(self, input_string: str):
+        """Parses single sentence in CNF form.
+        """
         clauses_list = input_string.split(self.and_splitter)
         return set(map(self.parse_clause, clauses_list))
 
     def parse_clause(self, input_string: str):
+        """Parses single clause.
+        """
         input_string = self.strip_brackets(input_string)
-        literals_list = input_string.split(self.or_splitter)
-        parsed_literals = set(map(self.parse_literal, literals_list))
+        literals_list = list(input_string.split(self.or_splitter))
+        parsed_literals = frozenset(map(self.parse_literal, literals_list))
         return Clause(parsed_literals)
 
-    def parse_literal(self, input_string):
+    def parse_literal(self, input_string: str):
+        """Parses single literal.
+        """
         negated = input_string.find(self.negate_marker) == 0
 
         if negated:
@@ -37,18 +47,21 @@ class ClauseParser(NamedTuple):
         name_arguments = input_string.split(self.arguments_open_bracket)
         literal_name = name_arguments[0]
         arguments = (name_arguments[1][:-1]).split(",")
-        parsed_arguments = list(map(self.parse_argument, arguments))
+        parsed_arguments = tuple(map(self.parse_argument, arguments))
 
         return Literal(negated, parsed_arguments, literal_name)
 
-    def strip_brackets(self, input_string):
+    def strip_brackets(self, input_string: str):
+        """Removes redundant brackets.
+        """
         if input_string.find(self.clause_open_bracket) == 0:
             input_string = input_string[len(self.clause_open_bracket):-len(self.clause_close_bracket)]
 
         return input_string
 
-    @staticmethod
-    def parse_argument(input_string):
+    def parse_argument(self, input_string: str):
+        """Parses single argument.
+        """
         if input_string[0].isupper():
             return Argument(input_string, True)
 
--- src/example.py
+++ src/example.py
@@ -1,4 +1,6 @@
 from clauseparser import ClauseParser
+from resolution_method import resolution
+
 
 input_string = """/PIES(x) OR WYJE(x)
 /POSIADA(x,y) OR /KOT(y) OR /POSIADA(x,z) OR /MYSZ(z)
@@ -6,6 +8,13 @@ input_string = """/PIES(x) OR WYJE(x)
 POSIADA(Janek,x) AND [KOT(x) OR PIES(x))]
 /KIEPSKO_SYPIA(Janek) AND POSIADA(Janek,z) AND MYSZ(z)"""
 
+thesis = "KIEPSKO_SYPIA(Janek) OR /POSIADA(Janek,z) OR /MYSZ(z)"
+
 clause_parser = ClauseParser()
 parsed_clauses = clause_parser.parse_cnf_list(input_string.split("\n"))
 print('\n'.join(' '.join(map(str, sl)) for sl in parsed_clauses))
+
+parsed_thesis = clause_parser.parse_clause(thesis)
+print(parsed_thesis)
+
+print(resolution(parsed_clauses,parsed_thesis))
